a145fafaef0322e52d270402e6e8a8c353454cbd
catharinascully <catharina.scully@gmail.com>
2024-12-04 15:14:21 -0300

    Update database.py

--- Vingadores3.0/model/database.py
+++ Vingadores3.0/model/database.py
@@ -58,3 +58,23 @@ class Database:
         except Error as e:
             print(f"Erro ao executar a query: {e}")
             return None
+        
+
+    def verificar_heroi_no_banco(nome_heroi):
+        try:
+            db = Database()
+            db.connect()
+            
+            query = "SELECT heroi_id FROM heroi WHERE nome_heroi = %s"
+            resultado = db.select(query, (nome_heroi,))
+            
+            db.disconnect()
+            
+            if resultado:
+                return resultado[0][0]
+            else:
+                return None
+        
+        except Exception as e:
+            print(f"Erro ao consultar herói no banco: {e}")
+            return None
